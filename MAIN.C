cca2ba2f8a2cde0be8bc994e4e4e1eebf9c4f5ec
Olga Koldachenko <okold525@mtroyal.ca>
2020-02-11 14:38:49 -0700

    Moved a couple funcitons, created makefile

--- MAIN.C
+++ MAIN.C
@@ -1,20 +1,9 @@
 #include <stdio.h>
 #include <linea.h>
 #include <osbind.h>
+#include "drawlib.h"
 
 #define BYTES_PER_SCREEN 32000
-
-#define PLAY_AREA_HEIGHT_PIXELS 384
-#define PLAY_AREA_WIDTH_PIXELS 160
-#define PLAY_AREA_HEIGHT 24
-#define PLAY_AREA_WIDTH 10
-#define PLAY_AREA_OFFSET_X 60
-#define PLAY_AREA_OFFSET_Y 5
-
-#define SOLID 0xFFFF
-#define DASHED 0xCCCC
-#define DASHED2 0x3333 
-#define XOR   2
 
 #define BLOCK_HEIGHT 16
 
@@ -22,15 +11,11 @@ typedef unsigned char UBYTE;
 typedef unsigned int  UWORD;
 typedef unsigned long ULONG;
 
-void draw_play_area();
 void disable_cursor();
 void fill_screen(ULONG *base, char pattern);
-void plot_line(unsigned short x1, unsigned short y1,
-               unsigned short x2, unsigned short y2,
-               short style, short mode);
+
 void print_bitmap(UWORD *base, int x, int y,
 				  const UWORD *bitmap, unsigned int height);
-			   
 
 int main()
 {
@@ -80,84 +65,7 @@ int main()
 }
 
 
-/* there might be a prettier way to do this idk */
-void draw_play_area()
-{
-	/* left border */
-	plot_line(PLAY_AREA_OFFSET_X - 8, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X - 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED, XOR);
-	
-        plot_line(PLAY_AREA_OFFSET_X - 6, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X - 6, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED2, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X - 5, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X - 5, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED, XOR);
 
-	plot_line(PLAY_AREA_OFFSET_X - 4, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X - 4, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED2, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X - 3, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X - 3, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED, XOR);
-
-	plot_line(PLAY_AREA_OFFSET_X - 2, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X - 2, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	SOLID, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X - 1, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X - 1, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	SOLID, XOR);
-	
-	/* right border */
-	plot_line(PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 1, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 1, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	SOLID, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 2, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 2, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	SOLID, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 3, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 3, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 4, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 4, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED2, XOR);
-
-	plot_line(PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 5, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 5, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 6, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 6, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED2, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 8, PLAY_AREA_OFFSET_Y,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	DASHED, XOR);
-
-	/* bottom border */
-	plot_line(PLAY_AREA_OFFSET_X - 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 1,
-	SOLID, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X - 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 2,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 2,
-	SOLID, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X - 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 3,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 3,
-	DASHED, XOR);
-	
-	plot_line(PLAY_AREA_OFFSET_X - 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 4,
-	PLAY_AREA_OFFSET_X + PLAY_AREA_WIDTH_PIXELS + 8, PLAY_AREA_OFFSET_Y + PLAY_AREA_HEIGHT_PIXELS + 4,
-	DASHED2, XOR);
-}
 
 void disable_cursor()
 {
@@ -174,21 +82,7 @@ void fill_screen(ULONG *base, char pattern)
 		*(loc++) = pattern;
 }
 
-void plot_line(unsigned short x1, unsigned short y1,
-               unsigned short x2, unsigned short y2,
-               short style, short mode)
-{
-	/* do line-a call to plot a line */
 
-	X1 = x1;
-	Y1 = y1;
-	X2 = x2;
-	Y2 = y2;
-	LNMASK = style;
-	WMODE = mode;
-	LSTLIN = 0;
-	linea3();
-}
 
 void print_bitmap(UWORD *base, int x, int y,
 				  const UWORD *bitmap, unsigned int height)
