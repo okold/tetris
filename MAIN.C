baa2ba81ca6e5524a25b6b9db45e59dec77444d1
Olga Koldachenko <okold525@mtroyal.ca>
2020-02-11 11:45:57 -0700

    Initial bitmap implementation.

--- MAIN.C
+++ MAIN.C
@@ -16,28 +16,65 @@
 #define DASHED2 0x3333 
 #define XOR   2
 
+#define BLOCK_HEIGHT 16
+
+typedef unsigned char UBYTE;
+typedef unsigned int  UWORD;
+typedef unsigned long ULONG;
+
 void draw_play_area();
 void disable_cursor();
-void fill_screen(char *base, char pattern);
+void fill_screen(ULONG *base, char pattern);
 void plot_line(unsigned short x1, unsigned short y1,
                unsigned short x2, unsigned short y2,
                short style, short mode);
+void print_bitmap(UWORD *base, int x, int y,
+				  const UWORD *bitmap, unsigned int height);
+			   
 
 int main()
 {
-	char *base = Physbase();
+	UWORD block_bitmap[BLOCK_HEIGHT] = 
+	{
+		0x8001,
+		0x4003,
+		0x2007,
+		0x1FFF,
+		0x100F,
+		0x100F,
+		0x100F,
+		0x100F,
+		0x100F,
+		0x100F,
+		0x100F,
+		0x100F,
+		0x1FF7,
+		0x3FFB,
+		0x7FFD,
+		0xFFFE
+	};
+
+	UBYTE *base8 = Physbase();
+	UWORD *base16 = Physbase();
+	ULONG *base32 = Physbase();
 
 	disable_cursor();          /* hide blinking text cursor */
 
-	fill_screen(base, -1);     /* set screen to all black */
+	fill_screen(base32, -1);     /* set screen to all black */
 	
 	linea0();		   /* needs this, otherwise linea3() will crash the program */
 	
 	draw_play_area();
 	
+	/* test tiled blocks */
+	print_bitmap(base16,0,0,block_bitmap,BLOCK_HEIGHT);
+	print_bitmap(base16,16,0,block_bitmap,BLOCK_HEIGHT);
+	print_bitmap(base16,0,16,block_bitmap,BLOCK_HEIGHT);
+	print_bitmap(base16,16,16,block_bitmap,BLOCK_HEIGHT);
+
 	Cnecin();                  /* wait for key press */
 
-	fill_screen(base, 0);      /* set screen to all white */
+	fill_screen(base32, 0);      /* set screen to all white */
 
 	return 0;
 }
@@ -128,10 +165,10 @@ void disable_cursor()
 	fflush(stdout);
 }
 
-void fill_screen(char *base, char pattern)
+void fill_screen(ULONG *base, char pattern)
 {
 	register int i = 0;
-	register char *loc = base;
+	register ULONG *loc = base;
 
 	while (i++ < BYTES_PER_SCREEN)
 		*(loc++) = pattern;
@@ -151,4 +188,16 @@ void plot_line(unsigned short x1, unsigned short y1,
 	WMODE = mode;
 	LSTLIN = 0;
 	linea3();
+}
+
+void print_bitmap(UWORD *base, int x, int y,
+				  const UWORD *bitmap, unsigned int height)
+{
+	int i;
+	int offset;
+	offset = (x>>4) + (y*40);
+	for (i = 0; i < height; i++)
+	{
+		*(base + offset + (40*i)) &= bitmap[i];
+	}		
 }
